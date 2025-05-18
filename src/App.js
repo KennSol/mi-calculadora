825fa8941a3e763d091d49e0bb7440b9dc0b2636
KennSol <kennethsolorzano113@gmail.com>
2025-05-17 22:49:29 -0700

    c1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from './components/Calculator';
+import './Calculator.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Calculadora React</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
